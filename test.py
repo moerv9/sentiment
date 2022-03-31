079cc95e077b42aa77a3458e4d4696ceb465376b
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-03-31 16:57:08 +0200

    #feature New tweet notebook with Listener Class #refactor config.py for env

---
+++ test.py
@@ -0,0 +1,50 @@
+import tweepy
+from config import ConfigAPI
+newconf = ConfigAPI()
+api = newconf.create_api("auth1")
+
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+    def __init__(self):
+        pass
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
+        listener = StdOutListener(fetched_tweets_filename)
+        stream = tweepy.Stream(newconf.getKeys()[0],newconf.getKeys()[1],newconf.getKeys()[2],newconf.getKeys()[3])
+
+        # This line filter Twitter Streams to capture data by the keywords: 
+        stream.filter(track=hash_tag_list)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
+class StdOutListener(tweepy.Stream):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+
+
+    def on_error(self, status):
+        print(status)
+
+# Authenticate using config.py and connect to Twitter Streaming API.
+if __name__ == '__main__':
+    hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
+    fetched_tweets_filename = "tweets.txt"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
