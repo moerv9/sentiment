b7de8a4cc27c2c22e3861a32a0b450be1c3eb2f6
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-04-09 14:43:07 +0200

    #streamlit i hate this

--- sent/tweets.py
+++ sent/tweets.py
@@ -11,7 +11,7 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S
 log = logging.getLogger()
 
 # Import Twitter API TOKEN
-sys.path.append('../')
+sys.path.append('./')
 from config import ConfigAPI
 newconf = ConfigAPI()
 api = newconf.create_api("auth1")
