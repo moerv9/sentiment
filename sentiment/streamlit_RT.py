57c2d495c12b8b723c6eed4df2ed9fea0124c9c2
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-05-26 19:06:29 +0200

    removed open style.css

--- sentiment/streamlit_RT.py
+++ sentiment/streamlit_RT.py
@@ -22,8 +22,8 @@ st.set_page_config(
     )
 st.subheader(f"Twitter Sentiment-Streaming for {date.today().strftime('%d-%m-%Y')}")
 
-with open("style.css") as f:
-    st.markdown(f"<style>{f.read()}</style",unsafe_allow_html=True)
+# with open("style.css") as f:
+#     st.markdown(f"<style>{f.read()}</style",unsafe_allow_html=True)
 
 ## FUNCTIONS
 def show_wordCloud(df):
