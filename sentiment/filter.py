61aea96a693ca671e10160efaa223d5d34efc58c
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-07-26 11:26:40 +0200

    filtered amp

--- sentiment/filter.py
+++ sentiment/filter.py
@@ -28,10 +28,12 @@ def cleanTweets(text):
     """
     text = re.sub(r'@[A-Za-z0-9]+',"",text,flags=re.IGNORECASE) #removes @mentions / r tells python that it is a raw stream (regex)
     text = re.sub(r'#[A-Za-z0-9]+',"",text, flags=re.IGNORECASE) #removes # 
+    text = re.sub(r'$[A-Za-z0-9]+',"",text, flags=re.IGNORECASE) #removes # 
     text = re.sub(r':',"",text,) #removes ':'
     text = demoji.replace(text, "") #removes emojis
     text = re.sub(r'\n+',"",text) #removes \n 
     text = re.sub(r'&amp;+',"",text) #removes &amp;
+    text = re.sub(r'amp;+',"",text) #removes amp;
     text = re.sub(r'RT[\s]+',"",text) #removes retweets
     #text = re.sub(r'https?:\/\/\S+',"",text) #removes hyperlink, the '?' matches 0 or 1 reps of the preceding 's'
     text = re.sub(r"http\S+","",text,flags=re.IGNORECASE)
