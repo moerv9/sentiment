1b8b6aa0e36250c4c591bb6638c8282c84b06449
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-08-02 15:41:29 +0200

    clean hashtags

--- sentiment/filter.py
+++ sentiment/filter.py
@@ -28,7 +28,7 @@ def cleanTweets(text):
         str: cleaned text
     """
     text = re.sub(r'@[A-Za-z0-9]+',"",text,flags=re.IGNORECASE) #removes @mentions / r tells python that it is a raw stream (regex)
-    #text = re.sub(r'#[A-Za-z0-9]+',"",text, flags=re.IGNORECASE) #removes # 
+    text = re.sub(r'#[A-Za-z0-9]+',"",text, flags=re.IGNORECASE) #removes # 
     text = re.sub(r':',"",text,) #removes ':'
     text = demoji.replace(text, "") #removes emojis
     text = re.sub(r'\n+',"",text) #removes \n 
