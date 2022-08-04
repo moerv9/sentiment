39fc566e6d0cacbd7c86a5d341ce88b6c017ded8
moerv9 <marvin.ottersberg@student.fh-kiel.de>
2022-08-04 17:20:51 +0200

    print tweet time. heroku error

--- sentiment/listener.py
+++ sentiment/listener.py
@@ -133,8 +133,9 @@ class StreamListener(tweepy.Stream):
                     time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
                     print(f"Total Tweets filtered: {self.amount_filtered}") 
                     #To check randomly what tweet is inserted to the DB at what time
+                    tweet_time = status.created_at.strftime("%Y-%m-%d %H:%M:%S %Z:%z")
                     if self.amount_filtered == 8:
-                        print(f"Tweet: '{items[0]}' inserted at {time_now}.")
+                        print(f"Tweet: '{items[0]}' (Time: {tweet_time}) inserted at localtime: {time_now}. User has {items[5]} followers.")
                     #self.tweet_list = []
                 self.amount_filtered = 0
 
